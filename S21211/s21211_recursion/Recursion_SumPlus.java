16a59f5a16c4e0138429fb3e39f22f681a6c2ed6
0taeho0 <166297377+0taeho0@users.noreply.github.com>
2024-06-13 09:58:16 +0900

    Create Recursion_SumPlus.java

---
+++ S21211/s21211_recursion/Recursion_SumPlus.java
@@ -0,0 +1,14 @@
+public class Recursion_SumPlus {
+	static int sumPlus(int num) {
+		System.out.println("ing : " + num);
+		if(num == 1) {
+			return 1;
+		} else {
+			return num + sumPlus(num-1);
+		}
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Sum : " + sumPlus(10));
+	}
+}
