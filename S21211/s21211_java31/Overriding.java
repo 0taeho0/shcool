26fbad209411e0260b662abec2785d66d960a932
0taeho0 <166297377+0taeho0@users.noreply.github.com>
2024-06-13 10:04:18 +0900

    Create Overriding.java

---
+++ S21211/s21211_java31/Overriding.java
@@ -0,0 +1,14 @@
+public class Overriding {
+
+	public static void main(String[] args) {
+		OverridingA a = new OverridingA();
+		a.cal(8);
+		
+		OverridingB b = new OverridingB();
+		b.cal();
+		
+		OverridingB bb = new OverridingB();
+		bb.cal();
+	}
+
+}
