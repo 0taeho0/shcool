d8663856ddee2c44419b4db5f1e1c63777e053b8
0taeho0 <166297377+0taeho0@users.noreply.github.com>
2024-06-13 09:58:48 +0900

    Create Resursion3_Power.java

---
+++ S21211/s21211_recursion/Resursion3_Power.java
@@ -0,0 +1,15 @@
+public class Resursion3_Power {
+	public static int power(int base, int exponent) {
+		if(exponent == 0) {
+			return 1;
+		} else {
+			return base * power(base, exponent-1);
+		}
+	}
+	
+	public static void main(String[] args) {
+		final int Base = 2;
+		final int Exponent = 8;
+		System.out.println(Base + "의 " + Exponent + "승 " + power(Base, Exponent));
+	}
+}
